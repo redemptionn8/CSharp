f760416a5d5595c51f7d5a4ef10593364fa3195a
Nate Miller <40781551+redemptionn8@users.noreply.github.com>
2019-09-14 12:18:44 -0400

    Start of Playing Card C Sharp files
    This project is to solidify concepts I learned from C Sharp class. The goal is to create classes that are universal among card games so that they can be built upon when creating multiple card games like different Hoyle games or CCG's like Sim City, MtG, Pokemon, etc.

---
+++ Deck.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class Deck
+{
+    protected int numOfCards { get; }
+    protected Array[] Cards { get; }
+    private Deck(int numOfCards, Array[] Cards)
+    {
+        this.numOfCards = numOfCards;
+        this.Cards = Cards;
+    }
+
+}
---
+++ Hoyle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class Hoyle
+{
+    protected int numOfCards { get; }
+    protected string suit { get; }
+    protected string cardValue { get; }
+    protected Hoyle(string suit, string cardValue)
+    {
+        this.suit = suit;
+        this.cardValue = cardValue;
+    }
+}
+
+public static class FullDeck
+{
+}
